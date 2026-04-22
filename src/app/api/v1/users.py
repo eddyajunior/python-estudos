e6cfb4bde96cb20f3dbb58986029964e1ebf5e90
Edson Amaral <eddyajunior@gmail.com>
2026-04-22 08:57:09 -0300

    Implementa injeção de dependência com Depends

--- src/app/api/v1/users.py
+++ src/app/api/v1/users.py
@@ -1,13 +1,17 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
 
 from app.models.user import UserCreate
 from app.services.user_service import UserService
 
 router = APIRouter(prefix="/api/v1/users", tags=["users"])
 
-user_service = UserService()
+
+def get_user_service() -> UserService:
+    return UserService()
 
 
 @router.post("/")
-def create_user(user: UserCreate):
+def create_user(
+    user: UserCreate, user_service: UserService = Depends(get_user_service)
+):
     return user_service.create_user(user)
